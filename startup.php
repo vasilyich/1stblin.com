14c616c375dd808af9f8c431f6e9b9a8067092fa
evgeniyVas <35341694+evgeniyVas@users.noreply.github.com>
2018-03-08 18:13:16 +0300

    Add files via upload

---
+++ startup.php
@@ -0,0 +1,18 @@
+<?php
+function startup()
+{
+	
+	$hostname = 'localhost';
+	$username = 'root';
+	$password = '';
+	$dbName   = 'a0181256_mymusic';
+	
+	setlocale(LC_ALL, 'ru_RU.CP1251');
+	
+	mysql_connect($hostname, $username, $password) or die('No connect with database!');
+	mysql_query('SET NAMES cp1251');
+	mysql_select_db($dbName) or die('No database');
+}
+
+
+?>
