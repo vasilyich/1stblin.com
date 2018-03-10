362319f57ba6bb0a4e5d46caef06803470f22b11
evgeniyVas <35341694+evgeniyVas@users.noreply.github.com>
2018-03-10 20:35:46 +0300

    Add files via upload

--- notes_tabs.php
+++ notes_tabs.php
@@ -6,17 +6,19 @@
         <meta charset="UTF-8">
         <meta name="description" content="Arrangements for Guitar Solo">
         <meta name="keywords" content="guitar, classical guitar, arrangement, notes, tabs">
-        <meta name="author" content="Alexey Vasilyev">        
+        <meta name="author" content="Alexey Vasilyev">     
+		<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>		
     </head>
+		<script src="js/select.js"></script>	
     <body>
     <div class="container">
       <header class="header">
       <nav class="topmenu">
-	<div class="flags">
-	  <a href="" title="Russian" class="langru"><img src="img/ru.png" alt="???????" ></a>	
-	  <a href="" title="English" class="langeng"><img src="img/en.png" alt="English" ></a>
-          <figure class="logo"><img src="img/logo.png" alt="logo" width="138" height="80"></figure>	
-	</div>
+	<select class="flags">
+  	  <option value="1" class="icon-en" selected></option>
+	  <option value="0" class="icon-ru"></option>
+	</select>
+          <figure class="logo"><img src="img/logo.png" alt="logo" width="138" height="70"></figure>	
         <ul class="menu">
             <li class="menu_item"><a href="index.html"><img src="img/home_icon.png" class="icon">&nbsp&nbspHome</a></li>
             <li class="menu_item"><a href="notes_tabs.php"><img src="img/music_sheet.png" class="musescoreicon">&nbsp&nbspMusic Scores</a></li>
@@ -25,7 +27,7 @@
         </ul>
       </nav>
       </header>
-      <div class="main_body">
+      <div class="main_body_music">
         <table id="songs" class="songstab">
 	<tr>
 		<th>No.</th>
@@ -36,7 +38,8 @@
 		<th>Watch Online</th>
 		<th>Buy</th>
 	</tr>
-<?php
+<!--  php code starts here -->
+	<?php
 	include("startup.php");
 	startup();
 	$result = mysql_query("SELECT * FROM  song");
@@ -45,31 +48,32 @@
 	
 	while($row = mysql_fetch_assoc($result))
 	{
-		echo "    <tr>\n";
-		echo "        <td>{$row['song_id']}</td>\n";
-		echo "        <td><img src='songs/{$row['song_id']}/pic.jpg'></td>\n";
-		echo "        <td>{$row['title_eng']}</td>\n";
-		echo "        <td>\n";
+		echo "<tr>\n";
+		echo "<td>{$row['song_id']}</td>\n";
+		echo "<td><img src='songs/{$row['song_id']}/pic.jpg'></td>\n";
+		echo "<td>{$row['title_eng']}</td>\n";
+		echo "<td>\n";
 		
 		$dif = $row['difficulty'];
 		for ($i = 0; $i < $dif; $i++) {
-			echo '            <img src="img/blue_star.png" class="blue_star">';
+			echo '<img src="img/blue_star.png" class="blue_star">';
 			echo "\n";
 		}
 			
-		echo "        </td>\n";
-		echo '        <td><a href="songDetail.php?id='.$row['song_id'].'">Info</a></td>';
+		echo "</td>\n";
+		echo '<td><a href="songDetail.php?id='.$row['song_id'].'">Info</a></td>';
 		echo "\n";
-		echo "        <td><a href='{$row['song_url']}' target='_blank'>Watch Online</a></td>\n";
-
-		if ( empty($row['buy_url']) ) {
-			echo "        <td>&nbsp</td>\n";
-		} else {
-			echo "        <td><a href='{$row['buy_url']}' target='_blank'>Buy</a></td>\n";
-		}
-		echo"    </tr>\n";
+		echo "<td><a href='{$row['song_url']}' target='_blank'>Watch Online</a></td>\n";
+		if ( empty($row['buy_url']) ) 
+			echo "<td>&nbsp</td>\n";
+		 else 
+			echo "<td><a href='{$row['buy_url']}' target='_blank'>Buy</a></td>\n";
+		
+		echo"</tr>\n";
 	}
-?>			
+?>				
+
+<!--  php code ends here -->
 	</table>
 	<br>
 	</div>
